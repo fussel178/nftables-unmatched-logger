d02dfe4418c5e8339d13c1a65fcdd2eb2f86ba4f
Ludwig Richter <riluzm@posteo.de>
2022-11-13 04:18:46 +0100

    add arch build

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # nftables-unmatched-logger
+
 Log unmatched nftables packets
+
+## Installation
+
+1. Clone this repository:
+
+   ```shell
+   git clone 'https://github.com/fussel178/nftables-unmatched-logger.git'
+   cd nftables-unmatched-logger
+   ```
+
+2. Build and install the package:
+
+   ```shell
+   makepkg -si
+   ```
+
+3. Install ulogd:
+
+   ```shell
+   sudo pacman -S ulogd
+   ```
+
+4. Enable plugins and configure logging stack in `/etc/ulogd.conf`:
+
+   ```ini
+   [global]
+   logfile="/var/log/ulogd.log"
+   loglevel=5
+   rmem=131071
+   bufsize=150000
+
+   ##
+   ## plugins
+   ##
+
+   # inputs
+   plugin="/usr/lib/ulogd/ulogd_inppkt_NFLOG.so"
+   plugin="/usr/lib/ulogd/ulogd_raw2packet_BASE.so"
+
+   # filters
+   plugin="/usr/lib/ulogd/ulogd_filter_IP2STR.so"
+   plugin="/usr/lib/ulogd/ulogd_filter_HWHDR.so"
+   plugin="/usr/lib/ulogd/ulogd_filter_IFINDEX.so"
+
+   # outputs
+   plugin="/usr/lib/ulogd/ulogd_output_JSON.so"
+
+   # JSON to python stack
+   #
+   stack=log1:NFLOG,base1:BASE,ifi1:IFINDEX,ip2str1:IP2STR,mac2str1:HWHDR,json1:JSON
+
+   ##
+   ## part configurations
+   ##
+
+   [log1]
+   group=1
+
+   [json1]
+   sync=1
+   mode="unix"
+   file="/run/nftables-unmatched-logger/ulog.sock"
+   ```
+
+5. Configure the log group in nftables:
+
+   ```shell
+   # after all commands in input chain
+   nft add rule inet filter input log prefix "nft_in_unmtch" group 1 limit rate 3/second
+
+6. Enable and start both systemd services:
+
+   ```shell
+   sudo systemctl enable --now nftables-unmatched-logger.service
+   sudo systemctl enable --now ulogd.service
+   ```
+
+7. Finished!
+
+## Query data
+
+To query data via console, you can use the `sqlite3` command line interface:
+
+```shell
+sudo sqlite3 -column -header /var/lib/nftables-unmatched-logger/main.sqlite "
+  SELECT ip_address,addresses.first_seen,addresses.last_seen,proto,port,name,count FROM calls 
+    INNER JOIN addresses ON addresses.id = calls.address_id 
+    INNER JOIN services ON services.id = calls.service_id 
+    ORDER BY ip_address;
+"
+```
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 import sqlite3
+from systemd import daemon
 import socket
 import sys
 import os
 import re
 import json
+import signal
 from json import JSONDecodeError
 
-database_path = "main.sqlite"
-unix_socket_path = "/tmp/collector.sock"
+database_path = "/var/lib/nftables-unmatched-logger/main.sqlite"
+unix_socket_path = "/run/nftables-unmatched-logger/ulog.sock"
 nft_log_prefix = "nft_in_unmtch"
 
 db_table_services = """ CREATE TABLE IF NOT EXISTS services (
@@ -97,93 +99,115 @@ def increment_call(db, address_id: int, service_id: int):
     cursor = db.cursor()
     cursor.execute("INSERT INTO calls(address_id, service_id, count) VALUES(?, ?, ?)", (address_id, service_id, 1))
 
+def register_exit_handler(handler):
+  for sig in (signal.SIGABRT, signal.SIGILL, signal.SIGINT, signal.SIGSEGV, signal.SIGTERM):
+    signal.signal(sig, handler)
+
 def main():
   print("Start process")
   db = None
   server = None
 
-  try:
-    print("Open SQLite database")
-    db = sqlite3.connect(database_path)
+  print("Open SQLite database")
+  sys.stdout.flush()
+  db = sqlite3.connect(database_path)
 
-    print("Create tables")
-    create_table(db, db_table_services)
-    create_table(db, db_table_addresses)
-    create_table(db, db_table_calls)
-
-    print("Open UNIX socket: " + unix_socket_path)
-    try:
-      os.unlink(unix_socket_path)
-    except OSError:
-      if os.path.exists(unix_socket_path):
-        raise
-    
-    server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    server.bind(unix_socket_path)
-    server.listen(1)
-
-    while True:
-      print("Waiting for ulog to connect...")
-      connection, _ = server.accept()
-
-      try:
-        print("Ulog is connected. Interpreting data...")
-        prepared = ""
-        while True:
-          raw = connection.recv(4096)
-          prepared += raw.decode("ascii")
-          parts = re.split(r'(?<=})\n(?={)', prepared)
-          prepared = ""
-          for part in parts:
-            try:
-              # netfilter packet
-              decoded = json.loads(part)
-
-              # skip wrong prefixed packet
-              if decoded["oob.prefix"] != nft_log_prefix:
-                continue
-
-              src_ip = decoded["src_ip"]
-              ip_protocol = decoded["ip.protocol"]
-              dest_port = decoded["dest_port"]
-
-              protocol_name = None
-              if ip_protocol == 6:
-                protocol_name = "tcp"
-              elif ip_protocol == 17:
-                protocol_name = "udp"
-              else:
-                # not a TCP or UDP packet -> ignoring (for now)
-                continue
-
-              service_id = upsert_service(db, dest_port, protocol_name)
-              address_id = upsert_address(db, src_ip)
-              increment_call(db, address_id, service_id)
-              db.commit()
-
-            except JSONDecodeError:
-              # incomplete JSON object -> store for next decoding round
-              prepared += part
-            except sqlite3.Error as e:
-              print(e)
-              print("Continue with next packet...")
-            except Exception as e:
-              print("Unhandled packet: {}".format(part))
-              raise e
-
-      finally:
-        print("Closing connection")
-        connection.close()
-
-  finally:
-    if server:
-      print("Close socket")
-      server.close()
-      os.unlink(unix_socket_path)
+  def close_db(signum, frame):
     if db:
       print("Close SQLite database")
-      db.commit()
+      sys.stdout.flush()
       db.close()
+  register_exit_handler(close_db)
+
+  print("Create tables")
+  sys.stdout.flush()
+  create_table(db, db_table_services)
+  create_table(db, db_table_addresses)
+  create_table(db, db_table_calls)
+
+  print("Open UNIX socket: " + unix_socket_path)
+  sys.stdout.flush()
+  try:
+    os.unlink(unix_socket_path)
+  except OSError:
+    if os.path.exists(unix_socket_path):
+      raise
+  
+  server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+  server.bind(unix_socket_path)
+  server.listen(1)
+
+  # register first handler
+  def close_server(signum, frame):
+    if server:
+      print("Close socket")
+      sys.stdout.flush()
+      server.close()
+    close_db(signum, frame)
+  register_exit_handler(close_server)
+
+  while True:
+    print("Waiting for ulog to connect...")
+    sys.stdout.flush()
+    daemon.notify("READY=1")
+    daemon.notify("STATUS=Waiting for ulog daemon to connect…")
+    connection, _ = server.accept()
+
+    def close_connection(signum, frame):
+      if connection:
+        print("Close ulog connection")
+        sys.stdout.flush()
+        connection.close()
+      close_server(signum, frame)
+    register_exit_handler(close_connection)
+
+    print("Ulog is connected. Interpreting data...")
+    sys.stdout.flush()
+    daemon.notify("STATUS=Ulog is connected. Interpreting data...")
+    prepared = ""
+    while True:
+      raw = connection.recv(4096)
+      prepared += raw.decode("ascii")
+      parts = re.split(r'(?<=})\n(?={)', prepared)
+      prepared = ""
+      for part in parts:
+        try:
+          # netfilter packet
+          decoded = json.loads(part)
+
+          # skip wrong prefixed packet
+          if decoded["oob.prefix"] != nft_log_prefix:
+            continue
+
+          src_ip = decoded["src_ip"]
+          ip_protocol = decoded["ip.protocol"]
+          dest_port = decoded["dest_port"]
+
+          protocol_name = None
+          if ip_protocol == 6:
+            protocol_name = "tcp"
+          elif ip_protocol == 17:
+            protocol_name = "udp"
+          else:
+            # not a TCP or UDP packet -> ignoring (for now)
+            continue
+
+          service_id = upsert_service(db, dest_port, protocol_name)
+          address_id = upsert_address(db, src_ip)
+          increment_call(db, address_id, service_id)
+          db.commit()
+
+        except JSONDecodeError:
+          # incomplete JSON object -> store for next decoding round
+          prepared += part
+        except sqlite3.Error as e:
+          print(e)
+          print("Continue with next packet...")
+          sys.stdout.flush()
+        except Exception as e:
+          print("Unhandled packet: {}".format(part))
+          sys.stdout.flush()
+          raise e
 
 if __name__ == '__main__':
   main()
